20c2bbb53c051c1b7439159d2bcec613dbbaf799
Jacob Sennett <jakesennett@me.com>
2017-09-23 20:12:23 -0500

    added stuff

---
+++ Java Arraylist Test/test.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.ArrayList;
+
+class test
+{
+    public static ArrayList<String> test = new ArrayList<String>();
+
+    public static void main(String [] args)
+    {
+        test.add("0");
+
+        printTest();
+    }
+
+    public static void printTest()
+    {
+        System.out.println(test.size());
+        System.out.println(test.get(0));
+        System.out.println((Integer.parseInt(test.get(0))+ 1);
+    }
+
+}
